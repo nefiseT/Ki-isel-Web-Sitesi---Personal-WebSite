f2f5eb337622cfbe99b153a710b7543767890314
g211210022 <104698212+g211210022@users.noreply.github.com>
2023-08-04 22:24:41 +0300

    Add files via upload

---
+++ kullanicilar.php
@@ -0,0 +1,9 @@
+<?php
+$user = "123@sakarya.edu.tr";
+$pass = "123";
+?>
+
+
+<footer class="py-5 bg-info text-white text-center">
+		Web-Teknolojileri Dersi Proje Ödevi © Nefise Türkoğlu 2023
+	</footer>
