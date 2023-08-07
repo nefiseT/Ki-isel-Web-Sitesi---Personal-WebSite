148360f978eb77128283cd3f3fc085264da74286
g211210022 <104698212+g211210022@users.noreply.github.com>
2023-08-07 18:11:18 +0300

    Delete kullanicilar.php

--- kullanicilar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = "123@sakarya.edu.tr";
-$pass = "123";
-?>
-
-
-<footer class="py-5 bg-info text-white text-center">
-		Web-Teknolojileri Dersi Proje Ödevi © Nefise Türkoğlu 2023
-	</footer>
