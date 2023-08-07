9f81104742b0cd2b1fc7548d5db1248b17f7c3f6
g211210022 <104698212+g211210022@users.noreply.github.com>
2023-08-07 19:02:52 +0300

    Add files via upload

---
+++ web teknolojileri web sitesi projesi/kullanicilar.php
@@ -0,0 +1,11 @@
+<title> Web Teknoloji Projesi Kişisel Web Sitesi Tasarımı </title>
+
+<?php
+$user = "123@sakarya.edu.tr";
+$pass = "123";
+?>
+
+
+<footer class="py-5 bg-info text-white text-center">
+		Web-Teknolojileri Dersi Proje Ödevi © Nefise Türkoğlu 2023
+	</footer>
